a5372c2efbce033fbc8ae321d178e4d128c940c6
Esraa Ismail <esraaismail309@gmail.com>
2022-07-15 20:39:26 +0200

    update some styles

--- src/layout/Registration.jsx
+++ src/layout/Registration.jsx
@@ -1,8 +1,6 @@
-import { TextField } from "@mui/material";
 import React from "react";
-import bg from "./../assets/big-data-conference-abstract-concept-vector-illustration-big-data-conference-abstract-concept-vector-illustration-innovative-idea-196907979.jpg";
+import bg from "./../assets/isometric-exhibition-demonstration-promo-stands-trade-stalls-expo-demonstration-stand-with-workers-visitors-vector-illustration-promo-stands-composition_229548-2111.webp";
 import logo from "./../assets/logo-tv-logo.png";
-
 import "./register.css";
 
 import { Form } from "../components/Form";
@@ -10,14 +8,10 @@ export const Registration = () => {
   return (
     <>
       <div className="container">
-        <div className="row justify-content-between">
-          <div className="col-md-6 position-relative">
-            <img src={bg} className="img-fluid" alt="register img" />
-            <img
-              src={logo}
-              alt="logo"
-              className="logo w-25 position-absolute "
-            />
+        <div className="row justify-content-between align-item-center">
+          <div className="col-md-6 position-relative mt-4">
+            <img src={bg} className="img-fluid " alt="register img" />
+            <img src={logo} alt="logo" className="logo position-absolute " />
           </div>
           <div className="col-md-6 mt-5  form__content">
             <div className="d-flex justify-content-between">
